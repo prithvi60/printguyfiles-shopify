a2ca70a6ad6d73abf05403b11702653e00cd972d
Prithvi <prithvin19@gmail.com>
2023-12-20 13:14:33 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -36,15 +36,43 @@ function App() {
   useEffect(() => {
     console.log("order data", orders);
   }, [orders]);
-  if(!orders){
-    return <div>Loading...</div>
-  }
 
-  const orderedfiles=objects.filter((obj)=> orders.find((a)=>obj.Key.split("-")[0].toLowerCase() === a.billing_address.first_name)).map((item)=>{
-    const order=orders[0].name
+  const orderedfiles=objects.filter((obj)=> orders.find((a)=>obj.Key.split("-")[0].toLowerCase() === a.billing_address.first_name.toLowerCase() )).map((item)=>{
+   
+    const order=orders.find((item2)=>{
+        // Parse date strings into Date objects
+const date1 = new Date(item2.created_at );
+const date2 = new Date(item.LastModified); 
+      // Calculate the difference in milliseconds
+const timeDifference = Math.abs(date2 - date1);
+
+// Convert milliseconds to days
+const daysDifference = timeDifference / (1000 * 60 * 60 * 24);
+return daysDifference <= 2 && item2.billing_address.first_name.toLowerCase() === item.Key.split("-")[0].toLowerCase()
+ && item2.line_items.find((a)=>a.title.split("-")[0] === item.Key.split("-")[1]) !== undefined
+})?.name ;
+
   return [{orderid:order},item]
   })
   console.log("manipulation",orderedfiles)
+
+  const modifiedOrderList=orderedfiles.sort((a, b) => {
+    // @ts-ignore
+return new Date(b[1].LastModified).getTime() - new Date(a[1].LastModified).getTime();
+})
+.filter((obj)=>obj[0].orderid !== undefined)
+;
+if(modifiedOrderList.length === 0){
+  if(orders.length === 0){
+    return <div style={{textAlign:"center",width:"100vw"}}><strong>Loading...</strong></div>
+  }else{
+  
+    return <div style={{textAlign:"center",width:"100vw"}}><strong>Customer&apos;s files will show here when order is placed.</strong></div>
+  }
+
+}
+
+
   return (
     <div className="App">
       <h2 style={{textAlign:"center"}}>Design Files from Customers</h2>
@@ -62,10 +90,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {orderedfiles.sort((a, b) => {
-            // @ts-ignore
-  return new Date(b[1].LastModified).getTime() - new Date(a[1].LastModified).getTime();
-}).map((o,idx) => {
+          {modifiedOrderList.map((o,idx) => {
             // Split the date string into an array of strings, using whitespace as the delimiter.
             const dateParts = String(o[1].LastModified).split(/\s+/);
             const specs= o[1].Key?.split("Specs:")[1]?.split(".")[0]?.split(",")
@@ -84,7 +109,7 @@ function App() {
                 <td style={{fontWeight:"bold",maxWidth:"200px",textAlign:"center"}}>{o[1].Key?.split("-")[0]}</td>
                 {/* <td>{o.Key}</td> */}
                 <td style={{fontSize:"small",textAlign:"center"}}>{o[1].Key?.split("-")[1]?.split(".")[0]}</td>
-                <td>{specs?.map((item,idx)=><li className={"specs"} key={idx}>{item.split("-")[0]} - <span style={{fontWeight:"bold"}}>{item.split("-")[1]}</span></li>)}</td>
+                <td style={{textAlign:"center"}}>{specs.length >1 ?specs?.map((item,idx)=><li className={"specs"} key={idx}>{item.split("-")[0]} - <span style={{fontWeight:"bold"}}>{item.split("-")[1]}</span></li>):"none"}</td>
                 <td  style={{textAlign:"center"}}>{`${day} ${month} ${year}`}</td>
                 <td style={{textAlign:"center"}}>{(Number(o[1].Size)/(1024 * 1024)).toFixed(3)}Mb</td>
                 <td>
